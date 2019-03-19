5bc898056f36db9722cd03d12e8583b65b32471c
John <john.rodler@lightspeedvt.com>
2019-03-19 15:41:03 -0700

    obfuscate admin emails

--- src/app/services/admin.guard.ts
+++ src/app/services/admin.guard.ts
@@ -3,6 +3,7 @@ import { CanActivate, Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
+import * as Crypto from 'crypto-js';
 
 import { SystemService } from './system.service';
 import { NotifyService } from './notify.service';
@@ -12,9 +13,9 @@ import { NotifyService } from './notify.service';
 })
 export class AdminGuard implements CanActivate {
   admins: Array<String> = [
-    'john.rodler@lightspeedvt.com',
-    'eric.mullen@lightspeedvt.com',
-    'henry.martinez@lightspeedvt.com'
+    'U2FsdGVkX19LzHUmUgpYZEN/gAxBFhw+G7doMLlxjm1Ux0bnnxIjAodZFO+wZ44k',
+    'U2FsdGVkX19s+bcPogElEiLnxOVpKrYppNoq+68ttZuXTB85N8CbP4WCIAhwiSOD',
+    'U2FsdGVkX1+zkFHYnK9JVBBgjkVvnKGnBp0nnV3/FADMeDc7W2EW+WRLPB7LrRpwr6QRtIxVMLRlaavoJaX+SQ=='
   ];
 
   constructor(
@@ -29,10 +30,12 @@ export class AdminGuard implements CanActivate {
       return this.afAuth.user.pipe(
         take(1),
         map(state => {
-          if (this.afAuth.auth.currentUser !== null && !(this.admins.indexOf(this.afAuth.auth.currentUser.email) >= 0)) {
-            return false;
-          }
-          return !!state;
+          const isAdmin = this.admins.find(admin => {
+            const crypto = Crypto.AES.decrypt(admin, 'dabeers');
+            const email = crypto.toString(Crypto.enc.Utf8).replace(/['"]+/g, '');
+            return this.afAuth.auth.currentUser.email === email;
+          });
+          return this.afAuth.auth.currentUser !== null && !isAdmin ? false : !!state;
         }),
         tap(hasAccess => {
           if (!hasAccess) {
@@ -42,7 +45,7 @@ export class AdminGuard implements CanActivate {
         })
       );
     }
-    return true; // FIXME: This is a temp hack to avoid auth flicker (https://goo.gl/GYGz3J)
+    return true;
   }
 
 }
