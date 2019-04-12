228855a4dd75af58e700bd9c6fd403c09d53255a
John <john.rodler@lightspeedvt.com>
2019-04-12 16:12:22 -0700

    cont. converting beers

--- src/app/services/beers.service.ts
+++ src/app/services/beers.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Subscription } from 'rxjs';
 
 import { NotifyService } from './notify.service';
@@ -10,7 +10,6 @@ import { IBeers, IBeer } from '../models/beer';
   providedIn: 'root'
 })
 export class BeersService implements OnDestroy {
-  beersCollection: AngularFirestoreCollection<IBeer>;
   beersDoc: AngularFirestoreDocument;
   beersSub: Subscription;
   beers: IBeers = {};
@@ -24,12 +23,12 @@ export class BeersService implements OnDestroy {
     this.resetActiveBeer();
     this.isLoading = true;
     this.beersDoc = this.afs.doc<IBeers>(`items/beers`);
-    this.beersDoc.ref.get()
-      .then(beers => {
-        this.beers = beers.data();
+    this.beersSub = this.beersDoc.valueChanges()
+      .subscribe(beers => {
+        this.beers = beers;
         this.isLoading = false;
-      })
-      .catch(error => {
+      },
+      error => {
         this.notify.error('Error fetching beers', error);
         this.isLoading = false;
       });
@@ -39,49 +38,69 @@ export class BeersService implements OnDestroy {
     this.beersSub.unsubscribe();
   }
 
+  private updateBeers(beers: IBeers) {
+    return this.beersDoc.set(beers, { merge: true })
+      .catch(error => this.notify.error('Error updating beer', error));
+  }
+
   public createBeer(title: String, description: String) {
-    // const beer: IBeer = {
-    //   id: 'beer-' + title.replace(/[^A-Za-z0-9]/g, '').trim().toLowerCase(),
-    //   title: title.trim(),
-    //   description: description.trim(),
-    //   image: '',
-    //   created: new Date(),
-    //   edited: new Date(),
-    //   isActive: true
-    // };
-    // return this.beersCollection.doc(`${beer.id}`)
-    //   .set(beer)
-    //   .catch(error => this.notify.error('Error creating beer', error));
-      // TODO: add beer to votes collection
+    const beer: IBeer = {
+      id: 'beer-' + title.replace(/[^A-Za-z0-9]/g, '').trim().toLowerCase(),
+      title: title.trim(),
+      description: description.trim(),
+      image: '',
+      created: new Date(),
+      edited: new Date(),
+      isActive: true
+    };
+    return this.beersDoc.ref.get()
+    .then(response => {
+      const beers = response.data();
+      beers[`${beer.id}`] = beer;
+      return this.updateBeers(beers)
+        .then(() => this.notify.success('Beer created successfully'));
+    })
+    .catch(error => this.notify.error('Error creating beer', error));
+    // TODO: add new beer doc to votes collection
   }
 
   public editBeer(title: String, description: String) {
-    // return this.beersCollection.doc(`${this.activeBeer.id}`)
-    //   .set({ title, description, edited: new Date() }, { merge: true })
-    //   .then(() => {
-    //     this.resetActiveBeer();
-    //     this.notify.success('Beer updated successfully');
-    //   })
-    //   .catch(error => this.notify.error('Error removing beer', error));
+    return this.beersDoc.ref.get()
+      .then(response => {
+        const beers = response.data();
+        beers[`${this.activeBeer.id}`].title = title;
+        beers[`${this.activeBeer.id}`].description = description;
+        beers[`${this.activeBeer.id}`].edited = new Date();
+        return this.updateBeers(beers)
+          .then(() => this.notify.success('Beer updated successfully'));
+      })
+      .catch(error => this.notify.error('Error editing beer', error));
   }
 
   public editBeerImage(image: String) {
-    // this.beersCollection.doc(`${this.activeBeer.id}`)
     //   .set({ image }, { merge: true })
     //   .then(() => this.notify.success('Beer updated successfully'))
     //   .catch(error => this.notify.error('Error removing beer', error));
   }
 
   public removeBeer(id: String) {
-    // return this.beersCollection.doc(`${id}`)
-    //   .set({ isActive: false }, { merge: true })
-    //   .catch(error => this.notify.error('Error removing beer', error));
+    return this.beersDoc.ref.get()
+      .then(response => {
+        const beers = response.data();
+        beers[`${id}`].isActive = false;
+        return this.updateBeers(beers);
+      })
+      .catch(error => this.notify.error('Error removing beer', error));
   }
 
   public restoreBeer(id: String) {
-    // return this.beersCollection.doc(`${id}`)
-    //   .set({ isActive: true }, { merge: true })
-    //   .catch(error => this.notify.error('Error restoring beer', error));
+    return this.beersDoc.ref.get()
+      .then(response => {
+        const beers = response.data();
+        beers[`${id}`].isActive = true;
+        return this.updateBeers(beers);
+      })
+      .catch(error => this.notify.error('Error restoring beer', error));
   }
 
   public setActiveBeer(id: String) {
